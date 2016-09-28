757e0cf817ef44c957113c33c75315d1aaffcfe3
valery <valery.fremaux@gmail.com>
2016-09-28 11:15:07 +0200

    automated sync from SVN

--- js/chronometer.js
+++ js/chronometer.js
@@ -1,54 +1,58 @@
 var dd = 0;
 var ss = 0;
+var s = {$this->time->seconds};
+var m = {$this->time->minutes};
+var h = {$this->time->hours};
+var j = {$this->time->days};
 var chrono;
 var started = 0;
 
 function startChronometer() {
-    if (started) {
-        return;
-    }
+    if (started) return;
     runChronometer();
 }
 
 function runChronometer() {
     started = 1;
 
-    if (m < 10) {
+    if (m < 10){
          mt = '0' + m;
     } else {
        mt = m;
     }
 
-    if (h < 10) {
+    if (h < 10){
         ht = '0' + h;
     } else {
         ht = h;
     }
 
-    if (s < 10) {
+    if (s < 10){
         st = '0' + s;
     } else {
         st = s;
     }
 
-    $('#chrono-display').html(j+' '+daytag+' '+ht+':'+mt+':'+st+' / ' + dd);
-    $('#chrono-display-sec').html(ss + ' / ' + dd + ' sec');
+    chronoId = document.getElementById(\"chronoDisplay\");
+    chronoId.innerHTML = j + ' $daytag ' + ht + ':' + mt + ':' + st + ' / ' + dd;
+    chronoIdSec = document.getElementById('chronoDisplaySec');
+    chronoIdSec.innerHTML = ss + ' / ' + dd + ' sec';
     dd++;
 
-    if (dd == 10) {
+    if (dd == 10){
         s = eval(s) + 1;
         ss++;
         dd = 0;
     }
-    if (s == 60) {
+    if (s == 60){
         s = 0;
         m++;
     }
-    if (m == 60) {
+    if (m == 60){
         m = 0;
         h++;
     }
-    if (h == 24) {
+    if (h == 24){
         h = 0;
         j++;
     }
@@ -57,24 +61,21 @@ function runChronometer() {
 }
 
 function stopChronometer(){
-    if (!started) {
-        return;
-    }
+    if (!started) return;
     window.clearTimeout(chrono);
     started = 0;
 }
 
 function resetChronometer(){
-    if (started) {
-        return;
-    }
+    if (started) return;
     s = 0;
     m = 0;
     j = 0;
     h = 0;
-    ss = 0;
+    ss = 0; 
     dd = 0;
-
-    $('#chrono-display').html("0 "+daytag+" 00:00:00 / 0");
-    $('#chrono-display-sec').html('00 / 0 sec');
+    chronoId = document.getElementById('chrono-display');
+    chronoId.innerHTML = \"0 $daytag 00:00:00 / 0\" ; 
+    chronoIdSec = document.getElementById('chrono-display-sec');
+    chronoIdSec.innerHTML = '00 / 0 sec';
 }
